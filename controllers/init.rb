aec1542eb8bb1c2de4935730e01400d769ff3ea6
yiweihuang <n1993212@gmail.com>
2016-07-25 02:50:31 +0800

    add init file

---
+++ controllers/init.rb
@@ -0,0 +1,4 @@
+require_relative 'base'
+require_relative 'account'
+require_relative 'folder_type'
+require_relative 'folder'
---
+++ services/init.rb
@@ -0,0 +1,4 @@
+require_relative 'find_authenticated_account'
+require_relative 'create_folder'
+require_relative 'get_owned_courses'
+require_relative 'get_owned_folder'
