82a9e3b97ed46e8d604c77dcfec103b06d0b80df
yiweihuang <n1993212@gmail.com>
2016-09-11 14:13:32 +0800

    modify route

--- services/get_course_slide.rb
+++ services/get_course_slide.rb
@@ -1,9 +1,9 @@
 require 'http'
 
 class GetCourseSlide
-  def self.call(current_uid:, auth_token:, course_id:)
+  def self.call(current_uid:, auth_token:, course_id:, folder_id:)
     response = HTTP.auth("Bearer #{auth_token}")
-                   .get("#{ENV['API_HOST']}/accounts/#{current_uid}/#{course_id}/folders/slides/segment")
+                   .get("#{ENV['API_HOST']}/accounts/#{current_uid}/#{course_id}/#{folder_id}/chapter/makekeyword")
     response.code == 200 ? response.parse : []
   end
 end
