a4d3445bbd9cbc462715d99c232e364e4b13d33a
yiweihuang <n1993212@gmail.com>
2016-09-13 11:39:57 +0800

    add new route

--- controllers/folders.rb
+++ controllers/folders.rb
@@ -50,7 +50,7 @@ class KeywordCloudApp < Sinatra::Base
     end
   end
 
-  get '/accounts/:uid/:course_id/:folder_type/:folder_id' do
+  get '/accounts/:uid/:course_id/subtitles/:folder_id' do
     if @current_uid && @current_uid.to_s == params[:uid]
       @course_id = params[:course_id]
       @folder_id = params[:folder_id]
@@ -59,17 +59,53 @@ class KeywordCloudApp < Sinatra::Base
                                        auth_token: session[:auth_token],
                                        course_id: @course_id,
                                        folder_id: @folder_id)
-      if @folder && @folder_type =="slides"
-        slim(:slide_folder)
-      elsif @folder && @folder_type =="concepts"
-        slim(:concept_folder)
-      elsif @folder && @folder_type =="subtitles"
+      if @folder
         @video_info = GetVideoContents.call(current_uid: @current_uid,
                                             auth_token: session[:auth_token],
                                             course_id: @course_id,
                                             folder_id: @folder_id,
                                             folder: @folder)
         slim(:subtitle_folder)
+      else
+        flash[:error] = '在您的帳號中，我們無法找到資料夾'
+        redirect "/accounts/#{params[:uid]}/#{params[:course_id]}/#{params[:folder_type]}"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/accounts/:uid/:course_id/slides/:folder_id' do
+    if @current_uid && @current_uid.to_s == params[:uid]
+      @course_id = params[:course_id]
+      @folder_id = params[:folder_id]
+      @folder_type = params[:folder_type]
+      @folder = GetFolderContents.call(current_uid: @current_uid,
+                                       auth_token: session[:auth_token],
+                                       course_id: @course_id,
+                                       folder_id: @folder_id)
+      if @folder
+        slim(:slide_folder)
+      else
+        flash[:error] = '在您的帳號中，我們無法找到資料夾'
+        redirect "/accounts/#{params[:uid]}/#{params[:course_id]}/#{params[:folder_type]}"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/accounts/:uid/:course_id/concepts/:folder_id' do
+    if @current_uid && @current_uid.to_s == params[:uid]
+      @course_id = params[:course_id]
+      @folder_id = params[:folder_id]
+      @folder_type = params[:folder_type]
+      @folder = GetFolderContents.call(current_uid: @current_uid,
+                                       auth_token: session[:auth_token],
+                                       course_id: @course_id,
+                                       folder_id: @folder_id)
+      if @folder
+        slim(:concept_folder)
       else
         flash[:error] = '在您的帳號中，我們無法找到資料夾'
         redirect "/accounts/#{params[:uid]}/#{params[:course_id]}/#{params[:folder_type]}"
