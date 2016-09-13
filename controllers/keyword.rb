1bdd016b7947af40cf4b55c471bc9b9e5e2f87b9
yiweihuang <n1993212@gmail.com>
2016-09-13 11:41:22 +0800

    make keyword

--- controllers/keyword.rb
+++ controllers/keyword.rb
@@ -1,28 +1,46 @@
 require 'sinatra'
+require 'json'
 
 # Click course and show three folders(concept,subtitle,slide)
 class KeywordCloudApp < Sinatra::Base
-  #顯示文字雲，點擊後，有幾個章節就有幾個文字雲按鈕
-  post '/accounts/:uid/:course_id/chapter/keyword' do
+  get '/keyword/:uid/:course_id/chapter' do
     if @current_uid && @current_uid.to_s == params[:uid]
-      @cid = params[:course_id]
+      @course_id = params[:course_id]
       @folder = GetOwnedFolder.call(current_uid: @current_uid,
                                     auth_token: session[:auth_token],
-                                    course_id: @cid,
+                                    course_id: @course_id,
                                     folder_type: "subtitles")
-      @course = GetCourseContents.call(current_uid: @current_uid,
+      @keyword = GetCourseContents.call(current_uid: @current_uid,
                                        auth_token: session[:auth_token],
                                        course_id: params[:course_id])
       @ordered_folder = @folder.sort_by { |chapter| chapter[:chapter_order] }
-      print(@ordered_folder)
       slim(:show_keywords)
-
     else
       slim(:home)
     end
   end
 
-  get '/accounts/:uid/:course_id/chapter/:chapter_id/makekeyword' do
+  get '/keyword/:uid/:course_id/chapter/:chapter_id/makekeyword' do
+    if @current_uid && @current_uid.to_s == params[:uid]
+      @auth_token = session[:auth_token]
+      @course_id = params[:course_id]
+      @chapter_id = params[:chapter_id]
+      @folder = GetOwnedFolder.call(current_uid: @current_uid,
+                                    auth_token: session[:auth_token],
+                                    course_id: @course_id,
+                                    folder_type: "subtitles")
+      @keyword = MakeKeyword.call(current_uid: @current_uid,
+                                  auth_token: @auth_token,
+                                  course_id: @course_id,
+                                  chapter_id: @chapter_id)
+      @ordered_folder = @folder.sort_by{ |chapter| chapter[:chapter_order] }
+      slim(:show_keywords)
+    else
+      slim(:home)
+    end
+  end
+
+  get '/keyword/:uid/:course_id/chapter/:chapter_id/showkeyword' do
     if @current_uid && @current_uid.to_s == params[:uid]
       @auth_token = session[:auth_token]
       @cid = params[:course_id]
@@ -31,7 +49,7 @@ class KeywordCloudApp < Sinatra::Base
                                     auth_token: session[:auth_token],
                                     course_id: @cid,
                                     folder_type: "subtitles")
-      @course = GetCourseSlide.call(current_uid: @current_uid,
+      @keyword = ShowKeyword.call(current_uid: @current_uid,
                                     auth_token: @auth_token,
                                     course_id: @cid,
                                     chapter_id: @chapter_id)
